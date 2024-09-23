5d1804a518f08eeaf4f10dabb8836521684198eb
Suvojit Kar <suvkar@cisco.com>
2024-09-24 02:04:57 +0530

    react material with navbar and card added with fetch custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import styled from '@emotion/styled';
+import CSCard from "./Components/CSCard/CSCard";
+import useFetch from "./Hooks/useFetch";
+import CSNavbar from './Components/CSNavbar/CSNavbar';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+  const {resp} = useFetch({url: "https://freetestapi.com/api/v1/books"});
+  if(resp?.length) {
+    return <><CSNavbar></CSNavbar><CardLayout>{resp.map((bookData: any) => <CSCard key={bookData.id} bookData={bookData}/>)}</CardLayout></>
+  }
 }
 
-export default App
+const CardLayout = styled.div `
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+  padding: 16px;
+`;
+
+export default App;
---
+++ src/Hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+interface FetchProps {
+    url: string;
+}
+
+const useFetch = (props: FetchProps) => {
+  const [resp, setResp] = useState([]);
+  const {url} = props;
+
+  useEffect(() => {
+    const getApi = async () => {
+      try {
+        const fetchResp = await fetch(url);
+        const resp = await fetchResp.json();
+        setResp(resp);
+      }
+      catch {
+        setResp([]);
+      }
+    }
+    getApi();
+  }, [url]);
+
+  return {resp};
+}
+
+export default useFetch
